b3354c7ea528f9667c8c011b8d9d000beb0689f0
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-02-03 11:33:42 +0000

    updated entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,19 @@ Member.create(name:"Andrea")
 Member.create(name:"Ana-Maria")
 
 # CREATE APPS: Outputs [{"1"=>col_1_value,"2"=>col_2_value},{..row2...},{...row3..}]
-apps_array_of_row_hashes = CSV.read('db/seed_fund_apps_v2.csv', :encoding => 'windows-1251:utf-8', :headers => true, :header_converters => :symbol, :converters => :all).map {|row| Hash[row.map{|row_head,row_val| [row_head,row_val]}]}
+apps_array_of_row_hashes = CSV.read('db/seed_fund_apps_v4.csv', :encoding => 'windows-1251:utf-8', :headers => true, :header_converters => :symbol, :converters => :all).map {|row| Hash[row.map{|row_head,row_val| [row_head,row_val]}]}
 apps_array_of_row_hashes.each { |row| App.create(row) }
-apps_array_of_row_hashes = CSV.read('db/seed_fund_apps_v2.csv', :encoding => 'windows-1251:utf-8', :headers => true, :header_converters => :symbol, :converters => :all).map {|row| Hash[row.map{|row_head,row_val| [row_head,row_val]}]}
 
 ## ASSOCIATE APPS WITH MEMBERS
 array_of_member_rows_per_app = CSV.read('db/member_assign.csv', :encoding => 'windows-1251:utf-8', :headers => false);
 array_of_member_rows_per_app.each_with_index { |row,index|
+  puts index
   current_app = App.find_by(id: index+1)
   current_app.members << Member.find_by(name: row[1])
   current_app.members << Member.find_by(name: row[2])
-  current_app.members << Member.find_by(name: row[3])
+  if row[3] != nil
+    current_app.members << Member.find_by(name: row[3])
+  end
 }
-
+# for each app in database
+  # add members from this row
